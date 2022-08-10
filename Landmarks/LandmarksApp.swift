fb8dd86b7c7436eabee6d9e6918d1e6900ee4294
kimjaeyeol <godbasic97@gmail.com>
2022-08-10 21:19:59 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by jaeyeol on 2022/08/10.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
